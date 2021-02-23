c6b0dbda55d8e165f97c71409b855de7ff5afbe4
Severin Gritsenko <severin@rnd.stcnet.ru>
2021-02-23 12:36:27 +0300

    added gh pages script

--- client.js
+++ client.js
@@ -2,11 +2,9 @@ import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <BrowserRouter>
-    <CssBaseline />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
-import { App } from "./App";
-import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
+import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
